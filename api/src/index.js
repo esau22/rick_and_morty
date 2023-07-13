59908649b84210d83ad1db1bac6043bf4e27b8fc
esau <emr.emr.espiritu@gmail.com>
2023-07-13 16:58:44 -0500

    feat back

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,9 @@
 const app = require("./app");
+const { conn } = require("./DB_connection"); // config de nuestra SYNC de ORM con la DB
 
 const PORT = 5020;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server raised in port: http://localhost:${PORT}`);
+  await conn.sync({ force: true });
 });
